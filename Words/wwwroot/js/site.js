92799e4b3095cddbfc3ea292a1f7b1cd50f546fd
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-05-07 22:35:14 +0200

    Lagt till möjlighet att välja ljust eller mörkt tema.

--- Words/wwwroot/js/site.js
+++ Words/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.getElementById('theme-toggle');
+    if (btn) {
+        btn.addEventListener('click', function () {
+            if (document.documentElement.classList.contains('dark-mode')) {
+                document.documentElement.classList.remove('dark-mode');
+                localStorage.setItem('theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark-mode');
+                localStorage.setItem('theme', 'dark');
+            }
+        });
+    }
+});
+
+
