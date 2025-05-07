d8331046b8bd48d0e52579a48266173d7e89dfc8
Pernilla Eldh <pernilla.eldh@gmail.com>
2025-05-07 18:43:21 +0200

    Lagt till möjligheter till kontakt.

--- Words/Pages/Privacy.cshtml.cs
+++ Words/Pages/Privacy.cshtml.cs
@@ -6,14 +6,18 @@ namespace Words.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration _configuration;
+        public string ContactEmail { get; set; }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            ContactEmail = _configuration["ContactEmail"];
         }
     }
 
